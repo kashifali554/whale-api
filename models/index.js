5ce1d199dfc5c8a78e1ffe964403c605e07c88eb
justincastilla <pxlperfection@gmail.com>
2017-03-09 10:28:28 -0800

    Jesse is eating orange slices

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 //head of the kraken
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/wwhowoowowowowoowowowoo");
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/wwhowoowowowowoowowowoo");
 
 // grabbing the whale schema and offering it up to outside files.
 module.exports.Whale = require("./whale.js");
